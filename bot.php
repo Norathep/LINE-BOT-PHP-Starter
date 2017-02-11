dfc7d5ca9eb5b61d6ffff233b726d2dc0f90fd01
Norathep <bankki1983@gmail.com>
2017-02-12 00:27:59 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,6 +1,6 @@
 <?php
 $access_token = 'phaEYrdmtLGy30cBJkK2zB9eke3JLwcgU0KSMRuII1f/c/2Ml8NxvxdXY0Z7BElsVR3CJOvbGeebyiBFtiFnzML4e14AA+aN88GeTdYCnLvjRLMrY+oWP5FoPyjeHSKau+s1NNv7gRRYQGzVFwwx2AdB04t89/1O/w1cDnyilFU=';
-$me = 'Bank';
+//$me = 'Bank';
 // Get POST body content
 $content = file_get_contents('php://input');
 // Parse JSON
@@ -12,7 +12,68 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			
+			if ($event['message']['text'] == 'stock') {
+				// Get replyToken
+				$replyToken = $event['replyToken'];
+		
+				$msg1 = [
+					'type' => 'text',
+					'label' => 'Buy',
+					'text' => 'Yes',
+				];
 
+
+				$msg2 = [
+					'type' => 'text',
+					'label' => 'Sell',
+					'text' => 'No',
+				];
+				
+				$act = array
+					(
+					[$msg1],
+					[$msg2],
+					);
+
+				$template = array( 
+					'type' => 'buttons',
+					//'thumbnailImageUrl' => 'https://dry-dawn-14913.herokuapp.com/screen.jpg',
+					'title' => 'Menu',
+					'text' => 'Please select',
+					'actions' => [$act],
+
+
+				);
+
+				// Build message to reply back
+				$messages = [
+					'type' => 'template',
+					'altText' => 'Which factor that you interest',
+					'template' => [$template],
+				];
+
+
+
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+			//$me = [$result];
+			echo $result . "\r\n";
+			} 
 				
 			// Get text sent
 			$text = $event['message']['text'];
