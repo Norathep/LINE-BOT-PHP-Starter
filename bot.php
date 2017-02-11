fc2c266571839c248dc6568c0a3ba8a0723fbac3
Norathep <bankki1983@gmail.com>
2017-02-12 00:20:36 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -83,7 +83,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => [$me],
+				'text' => [$text],
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
