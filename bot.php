03e47cd37ecd5d0329cae83a7e6122f1c72924ff
Norathep <bankki1983@gmail.com>
2017-02-11 22:07:32 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -41,7 +41,7 @@ if (!is_null($events['events'])) {
 			curl_close($ch);
 
 			echo $result . "\r\n";
-			}
+			} else {
 			// Get text sent
 			$text = $event['message']['text'];
 			// Get replyToken
@@ -72,6 +72,7 @@ if (!is_null($events['events'])) {
 			curl_close($ch);
 
 			echo $result . "\r\n";
+			}
 		}
 	}
 }
