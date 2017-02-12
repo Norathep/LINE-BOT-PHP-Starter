d773ebd3d0827e77f8ec42bf0a28279c73e77e70
Norathep <bankki1983@gmail.com>
2017-02-12 08:36:28 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,5 +1,6 @@
 <?php
 $access_token = 'phaEYrdmtLGy30cBJkK2zB9eke3JLwcgU0KSMRuII1f/c/2Ml8NxvxdXY0Z7BElsVR3CJOvbGeebyiBFtiFnzML4e14AA+aN88GeTdYCnLvjRLMrY+oWP5FoPyjeHSKau+s1NNv7gRRYQGzVFwwx2AdB04t89/1O/w1cDnyilFU=';
+$me = 'bank';
 // Get POST body content
 $content = file_get_contents('php://input');
 // Parse JSON
@@ -74,7 +75,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+				'text' => $me
 			];
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
