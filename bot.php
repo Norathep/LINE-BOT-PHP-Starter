f880bc5abc3311d568ff9dffe6386e5054e39eb5
Norathep <bankki1983@gmail.com>
2017-02-14 09:21:17 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -69,13 +69,13 @@ if (!is_null($events['events'])) {
 			}else {
 			
 			// Get text sent
-			$text = $event['message']['text'];
+			$text = $event['source']['userId'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $me
+				'text' => $text
 			];
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
